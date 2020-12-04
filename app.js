809f2dbd2cddc31790cf7041a6d6a1d300f6d10f
PratikBhusal <PratikBhusal@users.noreply.github.com>
2020-12-03 22:07:08 -0600

    TODO: Fix a bug where session variable for user login does not persist

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 var createError = require('http-errors');
 var express = require('express');
+var session = require('express-session');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var databaseRouter = require('./routes/database');
 
 var app = express();
+
+// Session Variable Middleware must be "used" before any routes are
+// declared/connected to the app.
+app.use(session({
+  secret: 'CS6314-TableTopStoreProject',
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,6 +28,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Create session variables if it does not already exist
+app.use((req, res, next) => {
+   // Check if we've already initialised a session
+   if (req.session.user_info === undefined) {
+      req.session.user_info = {};
+   }
+   next();
+});
+
+// Routes
 app.use('/', indexRouter);
 app.use('/database', databaseRouter);
 
@@ -26,6 +45,10 @@ app.use('/database', databaseRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+// app.use(session({
+//   secret: 'CS6314-TableTopStoreProject'
+// }));
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -37,5 +60,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,19 @@ const express = require('express');
 const router = express.Router();
 const crypto = require('crypto');
 
+const session = require('express-session');
+
 const mysql = require('mysql');
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "boardgame_shop"
+var sql_pool  = mysql.createPool({
+  connectionLimit : 10,
+  host            : 'localhost',
+  user            : 'root',
+  password        : 'root',
+  database        : 'boardgame_shop'
 });
+
+
+const hash_string = (str) => crypto.createHash('sha512').update(str.normalize()).digest('hex');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -40,11 +46,54 @@ router.get('/history', function(req, res) {
 router.get('/checkout', function(req, res) {
   // TODO: Prior to going to the checkout page, check if a user is signed in. If
   // not go to sign in page
+  console.log(req.session.user_info);
   res.render('checkout');
 });
 
 
-// Database {{{
+// Sign In {{{
+router.post('/signin/verify', function(req, res) {
+
+  sql_pool.getConnection(function(err, db) {
+    if (err) throw err;
+
+    var credentials_select_sql = "SELECT * FROM credential WHERE "
+      + "username='" + hash_string(req.body.username) + "' and "
+      + "password='" + hash_string(req.body.PASSWORD) + "'";
+    db.query(credentials_select_sql, function (err, result) {
+      if (err) throw err;
+
+      if (result.length != 1) {
+        // TODO: Find a way better to indicate invalid username/password pair.
+
+        res.redirect("/signin");
+        return;
+      }
+
+      req.session.user_info = {
+        "id" : result[0].user_id
+      };
+      var user_select_sql = "SELECT first_name, last_name FROM user WHERE "
+        + "id='" + req.session.user_info.id + "'";
+      db.query(user_select_sql, function (err, result) {
+        if (err) throw err;
+
+        // TODO: The session variable does not persist outside of this function
+        // callback. Find a way for the session variable to persist.
+        req.session.user_info.first_name = result[0].first_name;
+        req.session.user_info.last_name = result[0].last_name;
+
+      });
+
+    });
+    db.release();
+    res.redirect("/");
+  });
+
+});
+// Sign In }}}
+
+// Sign Up {{{
 router.post('/new/user', function(req, res) {
   if (req.body.PASSWORD.normalize() !== req.body.PASSWORD_check.normalize()) {
     throw "Passwords do not match!";
@@ -52,35 +101,27 @@ router.post('/new/user', function(req, res) {
     // return;
   }
 
-  // db.connect(function(err) {
-  //   if (err) throw err;
-  //   var sql = "INSERT INTO user SET ?";
-  //   db.query(sql, {"first_name" : req.body.first_name, "last_name" : req.body.last_name}, function (err, result) {
-  //     if (err) throw err;
-  //     console.log("INSERT YAY");
-  //     console.log(result.insertId);
-  //   });
-  // });
-
-  db.connect(function(err) {
-    if (err) throw err;
-
-    const hash_string = (str) => crypto.createHash('sha512').update(str.normalize()).digest('hex');
+  sql_pool.getConnection(function(err, db) {
+    if (err) throw err; // Unable to connect
 
     const username_hash = hash_string(req.body.username);
+    var user_insert_sql;
+
     db.query(
       "SELECT username FROM credential WHERE username='" + username_hash + "'",
       function (err, result) {
         if (err) throw err;
 
         if (result.length != 0) {
-          throw "Username already exists!"
+          throw "Username already exists!";
         }
 
-        var user_insert_sql = "INSERT INTO user "
+        user_insert_sql = "INSERT INTO user "
           + "(first_name, last_name) VALUES ('"
           + req.body.first_name + "', '"
           + req.body.last_name +"')";
+
+
         db.query(user_insert_sql, function (err, result) {
           if (err) throw err;
 
@@ -89,15 +130,18 @@ router.post('/new/user', function(req, res) {
             + username_hash + "', '"
             + result.insertId + "', '"
             + hash_string(req.body.PASSWORD) + "')";
-          db.query(credential_insert_sql, function (err) { if (err) throw err; });
+
+          db.query(credential_insert_sql, function (err) {
+            if (err) throw err;
+          });
         });
-      }
-    );
+    });
+
+    db.release();
   });
 
-  res.redirect('/');
+  return res.redirect('/');
 });
-// Database }}}
-
+// Sign Up }}}
 
 module.exports = router;
